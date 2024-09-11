cedbd2905a0569edea8db0ba7255fd34c575847d
Babita00 <babitaasth07@gmail.com>
2024-09-11 08:52:12 +0545

    whole table update

--- Search-Filter-Component/src/components/ProductTable.jsx
+++ Search-Filter-Component/src/components/ProductTable.jsx
@@ -1,42 +1,51 @@
-import ProductRow from "./ProductRow"
-import ProductCategoryRow from "./ProductCategoryRow"
-const ProductTable = ({products,searchText,inStockOnly}) => {
+import ProductRow from "./ProductRow";
+import ProductCategoryRow from "./ProductCategoryRow";
+import PropTypes from 'prop-types';
 
-  const rows=[];
+const ProductTable = ({ products, searchText, inStockOnly }) => {
+  const rows = [];
+  let lastCategory = null;
 
-  let lastCategory=null;
+  products.forEach((product) => {
+    if (product.name.toLowerCase().indexOf(searchText.toLowerCase()) === -1) {
+      return;
+    }
+    if (inStockOnly && !product.stocked) {
+      return;
+    }
 
-products.forEach((product) => {
-
-  if(product.name.toLowerCase().indexOf(filterText.toLowerCase())===-1){
-    return;
-  }
-  if(inStock && !product.stocked){
-    return;
-  }
-
-  if(product.category!==lastCategory){
-    rows.push(<ProductCategoryRow category={product.category} key={product.category}/>)
-  }
-  rows.push(<ProductRow product={product} key={product.name}/>);
-  lastCategory=product.category;
-});
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow category={product.category} key={product.category} />
+      );
+    }
+    rows.push(<ProductRow product={product} key={product.name} />);
+    lastCategory = product.category;
+  });
 
   return (
-   <table>
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Price</th>
-      </tr>
-    </thead>
-    <ttable>
-    <ProductCategoryRow categoty={"Fruits"}/>
-    <ProductRow/>
-    
-    </ttable>
-   </table>
-  )
-}
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+};
 
-export default ProductTable
+
+ProductTable.propTypes = {
+  products: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    price: PropTypes.string.isRequired,
+    stocked: PropTypes.bool.isRequired,
+    category: PropTypes.string.isRequired
+  })).isRequired,
+  searchText: PropTypes.string.isRequired,
+  inStockOnly: PropTypes.bool.isRequired
+};
+
+export default ProductTable;
