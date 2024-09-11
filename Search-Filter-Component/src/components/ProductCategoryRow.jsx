6067c985bf4dc7b36fc3a0c5a156f6e373b20cca
Babita00 <babitaasth07@gmail.com>
2024-09-11 08:51:53 +0545

    category update

--- Search-Filter-Component/src/components/ProductCategoryRow.jsx
+++ Search-Filter-Component/src/components/ProductCategoryRow.jsx
@@ -1,15 +1,15 @@
+import PropTypes from 'prop-types';
 
 
-const ProductCategoryRow = ({category}) => {
+const ProductCategoryRow = ({ category }) => {
   return (
-    <>
     <tr>
       <th colSpan="2">{category}</th>
-    
     </tr>
-    
-    </>
-  )
-}
+  );
+};
 
-export default ProductCategoryRow
+export default ProductCategoryRow;
+ProductCategoryRow.propTypes = {
+  category:PropTypes.string.isRequired,
+}
